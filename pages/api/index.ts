117280719f0ac79f7e84ec0418098e083e5108c1
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-09-26 18:01:17 +0530

    Fix: Updated Allowed Header

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -42,10 +42,7 @@ const allowCors =
       "Access-Control-Allow-Methods",
       "GET,OPTIONS,PATCH,DELETE,POST,PUT"
     );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-    );
+    res.setHeader("Access-Control-Allow-Headers", "*");
     if (req.method === "OPTIONS") {
       res.status(200).end();
       return;
