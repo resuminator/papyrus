ffae8f4deb3e5b9dfccdc29a0b6dc2998fd9eefa
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-09-26 17:56:25 +0530

    Added Allow CORS

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -21,10 +21,39 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getBrowserInstance } from "../../src/browser";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+const allowCors =
+  (fn: (req: any, res: any) => any) =>
+  async (
+    req: { method: string },
+    res: {
+      setHeader: (arg0: string, arg1: string | boolean) => void;
+      status: (arg0: number) => {
+        (): any;
+        new (): any;
+        end: { (): void; new (): any };
+      };
+    }
+  ) => {
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    );
+    if (req.method === "OPTIONS") {
+      res.status(200).end();
+      return;
+    }
+    return await fn(req, res);
+  };
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const APP = process.env.NEXT_PUBLIC_R8;
   if (!APP) return res.status(500).json({ message: "Invalid Config" });
 
@@ -92,4 +121,6 @@ export default async function handler(
       await browser.close();
     }
   }
-}
+};
+
+export default allowCors(handler);
